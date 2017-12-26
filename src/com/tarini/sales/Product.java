6fb17b893cd88da35addbca088b4f52dbdd5676c
suman <suman@tardins.com>
2017-12-26 20:22:12 +0530

    Initial Commit
    Initial commit for product Interface.
    
    Change-Id: Id8eb04424b9e681a6f8afb85de28967161dea76e

---
+++ src/com/tarini/sales/Product.java
@@ -0,0 +1,15 @@
+
+package com.tarini.sales;
+
+public interface Product {
+
+	public String getProductName();
+	
+	public String getProductDescription();
+	
+	public String getProductPrice();
+	
+	public String getProductTaxType();
+	
+	public Double getActualPrice();
+}
